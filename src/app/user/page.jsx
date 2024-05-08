703c3e18b632b2ed601973b8e77b5fec6294324c
intern_duyla <laianhduy04042001@gmail.com>
2024-05-08 14:38:24 +0700

    first commit

---
+++ src/app/user/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '@/components/Header/page'
+import Footer from '@/components/Footer/page'
+const page = () => {
+  return (
+    <div>
+        <Header />
+        <Footer />
+    </div>
+  )
+}
+
+export default page
---
+++ src/components/config.js
@@ -0,0 +1,23 @@
+export const categorys = [
+    {
+        title: 'Tuyến đường',
+        path:'1'
+    },
+    {
+        title: 'Khám phá',
+        path:'1'
+    },
+    {
+        title: 'Dịch vụ',
+        path:'1'
+    },
+    {
+        title: 'Về chúng tôi',
+        path:'1'
+    },
+    {
+        title: 'Hỗ trợ',
+        path:'1'
+    },
+
+]

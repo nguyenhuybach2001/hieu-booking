fab361af15571a119c1b6b59eefa8d4772525a78
Nguyen Huy Bach <bachbom27@gmail.com>
2024-05-08 21:53:08 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,12 @@
+import Search from "@/components/search";
 
 export default function Home() {
-  return <div className="text-3xl mt-20">Content</div>;
+  return (
+    <div className="bg-[url('/images/image.png')] bg-cover">
+      <div className="max-w-6xl text-white w-full mx-auto px-12 ">
+        Content
+        <Search />
+      </div>
+    </div>
+  );
 }
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,22 +1,40 @@
-'use client'
+"use client";
 import React from "react";
 import { categorys } from "./config";
 import Link from "next/link";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
+import { Button } from "antd";
 
 export default function Header() {
-  const router = useRouter()
-  return <div>
-  <div className="container flex justify-center items-center text-xl font-medium mt-5">
-    <div className=" mx-auto font-bold text-4xl">Logo</div>
-    <div className="">{categorys.map((category, i) => (
-      <Link key={i} href={category.path} className="mx-10 cursor-pointer">{category.title}</Link>
-    ))}</div>
-    <div className="flex gap-10 mx-10">
-      <button className="border-[2px] text-blue-600 border-blue-500 px-5 p-1 rounded-md cursor-pointer active:bg-blue-300" onClick={() => router.push('/register')}>Đăng ký</button>
-      <button className="border-[2px] text-blue-600 border-blue-500 px-5 p-1 rounded-md cursor-pointer active:bg-blue-300" onClick={() => router.push('/login')}>Đăng nhập</button>
+  const router = useRouter();
+  return (
+    <div>
+      <div className="max-w-6xl  w-full mx-auto px-12 ">
+        <div className="grid grid-cols-8 h-[105px] items-center">
+          <div className="col-span-2 font-bold text-5xl">Logo</div>
+          <div className="col-span-4 flex gap-4">
+            {categorys.map((category, i) => (
+              <Link key={i} href={category.path}>
+                {category.title}
+              </Link>
+            ))}
+          </div>
+          <div className="flex gap-10 col-span-2">
+            <Button
+              className="h-11"
+              // onClick={() => router.push("/register")}
+            >
+              Đăng ký
+            </Button>
+            <button
+              className="border-[2px] text-blue-600 border-blue-500 px-5 p-1 rounded-md cursor-pointer active:bg-blue-300"
+              onClick={() => router.push("/login")}
+            >
+              Đăng nhập
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  
-  </div>;
+  );
 }

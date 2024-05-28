a122024df1fb1a5c288beef8d1e123baaf492a02
Nguyen Huy Bach <bachbom27@gmail.com>
2024-05-28 11:14:48 +0700

    add api

---
+++ src/api/hostApi.js
@@ -0,0 +1,21 @@
+import { ENDPOINTS } from "@/config/endpoint";
+import axiosClient from "./axiosClient";
+
+export const hostApi = {
+  getDetailHostByHostId: () => () => {
+    return axiosClient.post(ENDPOINTS.GET_DETAIL_HOST);
+  },
+
+  createHost: () => () => {
+    return axiosClient.post(ENDPOINTS.CREATE_HOST);
+  },
+  updateRoom: () => (data) => {
+    return axiosClient.post(ENDPOINTS.UPDATE_ROOM, data);
+  },
+  getRoomByHostId: () => () => {
+    return axiosClient.get(ENDPOINTS.GET_ROOM_BY_HOST_ID);
+  },
+  addRoom: () => (data) => {
+    return axiosClient.post(ENDPOINTS.ADD_ROOM, data);
+  },
+};
---
+++ src/config/endpoint.js
@@ -0,0 +1,8 @@
+export const ENDPOINTS = {
+  //endpoint host
+  UPDATE_ROOM: "/nha/chinh-sua-nha",
+  ADD_ROOM: "/nha/them-nha",
+  GET_ROOM_BY_HOST_ID: "/nha/lay-nha-theo-chu-nha-id",
+  GET_DETAIL_HOST: "/nguoi-cho-thue/lay-nguoi-cho-thue-theo-id",
+  CREATE_HOST: "/nguoi-cho-thue/them-nguoi-cho-thue",
+};
